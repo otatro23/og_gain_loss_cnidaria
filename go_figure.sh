bbdda4b86a22359fbaacce61be44983d7f219884
otatro23 <110267754+otatro23@users.noreply.github.com>
2025-12-09 10:36:27 -0500

    Update go_figure.sh
    fixed indentation error

--- go_figure.sh
+++ go_figure.sh
@@ -3,9 +3,9 @@
 for dir in go_figure_tsvs*/* ; do 
 	echo $dir
 	gofigure.py -i $dir/BP_gain.tsv -j standard-plus -o $dir/gain -n bpo -e 100 -s user -su True
-        gofigure.py -i $dir/BP_loss.tsv -j standard-plus -o $dir/loss -n bpo -e 100 -s user -su True
+    gofigure.py -i $dir/BP_loss.tsv -j standard-plus -o $dir/loss -n bpo -e 100 -s user -su True
 	gofigure.py -i $dir/CC_gain.tsv -j standard-plus -o $dir/gain -n cco -e 100 -s user -su True
-        gofigure.py -i $dir/CC_loss.tsv -j standard-plus -o $dir/loss -n cco -e 100 -s user -su True
+	gofigure.py -i $dir/CC_loss.tsv -j standard-plus -o $dir/loss -n cco -e 100 -s user -su True
 	gofigure.py -i $dir/MF_gain.tsv -j standard-plus -o $dir/gain -n mfo -e 100 -s user -su True
 	gofigure.py -i $dir/MF_loss.tsv -j standard-plus -o $dir/loss -n mfo -e 100 -s user -su True
 done
