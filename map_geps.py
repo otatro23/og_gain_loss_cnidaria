69009d768e40a06c5383201317176820b1ad008c
otatro23 <110267754+otatro23@users.noreply.github.com>
2025-12-18 15:34:42 -0500

    Update map_geps.py
    Added code to extract highly expressed OGs from the og x gep expression matrices. Need to find a better way to create the og x geps matrices because when my code runs on the whole dataset, pandas gives an error about making too many columns individually. I can potentially just add everything to a dictionary and then convert to pandas df later.

--- map_geps.py
+++ map_geps.py
@@ -20,12 +20,10 @@ except IOError:
 
 # ------------------------------------- read in gene spectra files  -------------------------------------
 
-# goal:
-# I want to get a dictionary with keys acropora_1... for each gep. The values will be a dictionary with OG:expression z score, but for now, it will simply be gene:expression z score
+# dictionary of taxon:gep x og dataframe
+gep_spectra = {}
 
-geps = {}
-
-for file in os.scandir("test"):
+for file in os.scandir("z_score_gene_spectra"):
     taxon = file.name.split("_")[0]
     print("Taxon:", taxon)
 
@@ -43,7 +41,7 @@ for file in os.scandir("test"):
     for gene in genes:
         total_genes += 1
         try:
-            og = ogmap[taxon + "-" + gene]
+            og = "orthogroup:" + ogmap[taxon + "-" + gene]
             ogs.setdefault(og, [])
             ogs[og].append(gene)
         except KeyError as key:
@@ -51,30 +49,68 @@ for file in os.scandir("test"):
             missed_genes += 1
     print(f"missed genes: {missed_genes}    total genes: {total_genes}")
 
+    og_spectra = {}
+
+    # adds the expression of each gene in an orthogroup to the orthogroup column in spectra
     for og, genes in ogs.items():
-        spectra[og] = 0
+        #spectra[og] = 0
         for gene in genes:
-            spectra[og] += spectra[gene]
+            #spectra[og] += spectra[gene]
 
-    print(spectra)
+    new_df = {}
 
-    continue
+    for col in spectra.columns:
+        if col.startswith("orthogroup:"):
+            # removes the "orthogroup:" from the OG columns
+            #spectra[col.split(":")[1]] = spectra[col]
+            new_df[col.split(":")[1]] = spectra[col]
 
-    for gep_index in range(len(spectra)):
+        elif col.startswith("Unnamed"):
+            #renames gep column
+            #spectra["gep"] = spectra[col]
+            new_df["gep"] = spectra[col]
+        else:
+            #spectra.pop(col)
+            pass
 
-        for gene, value in spectra.iloc[gep_index].items():
-            #print(f"Index: {index}, Value: {value}")
-            if gene.startswith("Unnamed"):
-                gep_name = taxon + "_" + str(int(value))
-                print(gep_name)
-                geps[gep_name] = {}
-            else:
-                try:
-                    og = ogmap[taxon + "-" + gene]
-                    print(og)
-                    geps[gep_name].setdefault(og, 0)
-                    geps[gep_name][og] += value
-                except KeyError as key:
-                    print(f"Key {key} not found in ogmap.")
+    print(new_df)
+
+    gep_spectra[taxon] = pd.DataFrame(new_df)
+
+
+# input: taxon name and its gep-spectra df
+# output: dictionary of geps and sets of upregulated orthogroups
+def get_ogs(taxon, df):
+    high_ogs = {} # keys: gep     values: set of OGs upregulated
+
+    for index in range(len(df)):
+        row = df.iloc[index]
+        gep = taxon + "_" + str(int(row["gep"]))
+        og_exp = row.drop("gep")
+        og_exp = og_exp[og_exp > 0] # filter for expression zscore over 0 (upregulated OGs)
+        high_ogs[gep] = set(og_exp.index) # get the ogs that got by the filtering step
+
+    return high_ogs
+
+ogs = {} # key: taxon     {value: set(upregulated ogs)}
+
+for taxon, df in gep_spectra.items():
+    ogs[taxon] = get_ogs(taxon, df)
+
+def jaccard(set1, set2):
+    return len(set1.intersection(set2)) / len(set1.union(set2))
+
+# input dictionaries of gep:set(og) for 2 taxa
+# output: matrix of jaccard indices of OG sets. Rows are taxon1; cols are taxon2.
+def compare(geps_ogs1, geps_ogs2):
+    jaccards = pd.DataFrame(index = geps_ogs1.keys(), columns = geps_ogs2.keys())
+    print(jaccards)
+    for gep1, og_set1 in geps_ogs1.items():
+        for gep2, og_set2 in geps_ogs2.items():
+            jaccards.loc[gep1, gep2] = jaccard(og_set1, og_set2)
+    print(jaccards)
+
+compare(ogs["aurelia"], ogs["nematostella"])
+
 
 
